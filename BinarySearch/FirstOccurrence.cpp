fe88a54d5088b67a4d0de5162ac72f8939ad58dc
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-17 12:45:54 +0800

    Update FirstOccurrence.cpp
    叫好的版本 能維持BS的logn效能

--- BinarySearch/FirstOccurrence.cpp
+++ BinarySearch/FirstOccurrence.cpp
@@ -4,23 +4,23 @@ int findFirstOccurrence(vector<int> nums, int target) {
     int left=0;
     int right=n-1;
     int mid=0;
-    if(n==0 ){
-        return -1;
-    }
+    //!!!!!直接先設定-1是精髓
+    int answer=-1;
+
     while(left<=right){
         mid=(left+right)/2;
         if(target==nums[mid]){
-            //這邊寫法是可以 但效能不好 
-            while(mid>0&&nums[mid]==nums[mid-1]){
-                mid--;
-            }
-            return mid;
+            
+            //先把答案存起來
+            answer=mid;
+            
+            //再繼續往左找即可 不用想太多
+            right=mid-1;
         }else if(target>nums[mid]){
             left=mid+1;
         }else{
             right=mid-1;
         }
     }
-
-    return -1;
+    return answer;
 }
