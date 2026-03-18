56cf458974652abbc54126450b43dc63ca07fa13
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-18 13:36:28 +0800

    Create NestedBrackets.cpp

---
+++ stack/NestedBrackets.cpp
@@ -0,0 +1,6 @@
+//Validate Properly Nested Brackets
+//Given a string, check if all brackets ('()', '{}', '[]') are properly matched and nested. Return 1 if valid, otherwise return 0.
+
+//解題思路
+//只要看到「成雙成對」、「後進先出」、「對稱檢查」，腦袋就要跳出：Stack 。
+
