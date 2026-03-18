ca5fa8c2b67ef266b573e4d77dd7b008f7936291
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-18 15:27:16 +0800

    Create Min-TrackingStack.cpp
    暫時存檔

---
+++ stack/Min-TrackingStack.cpp
@@ -0,0 +1,4 @@
+//Min-Tracking Stack Implementation
+//Implement a stack that supports push, pop, top, and getMin operations in O(1) time, where getMin returns the minimum element.
+
+//主題是min track 且題目有說要O(1) 所以要選擇用Two Stacks去解決
