04cbd9f5a041a60e728931a1a96ad91106861665
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-18 13:01:44 +0800

    Create greedyAlgorithm.cpp
    好難

---
+++ GreedyAlgo/greedyAlgorithm.cpp
@@ -0,0 +1,33 @@
+//easy
+//Maximum Number of Non-Overlapping Intervals
+//Given an array of intervals where each interval has a start and end time, return the maximum number of non-overlapping intervals.\
+//0 <= meetings.length <= 1000   0 <= meetings[i][0] < meetings[i][1] <= 10^9
+
+//[[1,2], [3,4]]，meetings[0] 就是 [1,2]，而 meetings[0][0] 就是 1（開始時間）
+int maximizeNonOverlappingMeetings(vector<vector<int>> meetings) {
+    int n=meetings.size();
+    int count=0;
+  
+    // record current end ,-1 because meeting can start at 0
+    int last_end=-1;
+    
+    if(n==0){
+        return 0;
+    }
+    
+    //sort the meetings by end ,use lambda 注意這邊的語法 非常重要 這邊a[1 2],我們取a[1]就是end time
+    sort(meetings.begin(),meetings.end(),[](const vector<int> &a,const vector<int> &b)  { return a[1]<b[1]; });
+    
+    for(int i=0;i<n;i++){
+        int start=meetings[i][0];
+        int end=meetings[i][1];
+
+        //只要我的開始時間>=目前的結束時間即可
+        if(start>=last_end){
+            count++;
+            last_end=end;
+        }
+    }
+    return count;
+}
+
