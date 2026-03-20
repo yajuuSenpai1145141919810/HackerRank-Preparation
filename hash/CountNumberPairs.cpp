a7e219699d2e16707c6eb74920e46d90b1c78676
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-20 21:43:21 +0800

    Create CountNumberPairs.cpp
    topics

---
+++ hash/CountNumberPairs.cpp
@@ -0,0 +1,3 @@
+// Count Number Pairs
+// Given a sorted array of positive integers and a target value, count the number of pairs (i, j) where i < j and array[i] + array[j] <= target.
+// Input: prices = [1, 2, 3, 4, 5], budget = 7 => Output: 8
