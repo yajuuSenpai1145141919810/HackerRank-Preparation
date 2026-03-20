05d4b1723e03ae68b21b642d20202e798d392ffc
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-21 00:17:43 +0800

    Create TwoSum.cpp
    topic done

---
+++ Hash/TwoSum.cpp
@@ -0,0 +1,7 @@
+// Two Sum
+// Given an array of positive integers and a target integer, 
+// return the indices of two elements that sum to the target or [-1, -1] if no such pair exists.
+
+// Input:taskDurations = [2, 7, 11, 15] ,slotLength = 9, Output:[0, 1]
+
+
