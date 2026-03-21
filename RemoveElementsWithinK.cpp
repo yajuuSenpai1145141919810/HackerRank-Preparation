4e9bcb0c2acc216d39ae76792155a8b175e39182
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-21 16:51:10 +0800

    Create RemoveElementsWithinK.cpp
    topic done

---
+++ RemoveElementsWithinK.cpp
@@ -0,0 +1,10 @@
+// Easy
+// Remove Elements Within K Distance
+
+// Given a non-decreasing array of integers and an integer K, 
+// remove in-place any element that is within K of the previous kept element and return the new length. 
+// Use constant extra space and single pass with two pointers.
+
+//Input: timestamps = [1, 2, 3, 8, 10] K = 3 Output: 2
+
+ 
