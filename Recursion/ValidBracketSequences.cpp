71e724362a0e089863e7c08cb5d5b25a02e96e00
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-22 18:46:51 +0800

    Update ValidBracketSequences.cpp

--- Recursion/ValidBracketSequences.cpp
+++ Recursion/ValidBracketSequences.cpp
@@ -2,5 +2,6 @@
 // Given n, return all valid sequences of n pairs of '<' and '>' with proper nesting.
 // Input: n = 2 Output: <><> and <<>>
 
-//這題跟Validate Brackets那一題目差在 這題只有一種 所以不要用stack
+// 這題跟Validate Brackets那一題目差在 這題只有一種 所以不要用stack
+// 不能用 while 的原因只有一個：while 只能走出一條直線，但這題是一棵樹!!!
 
