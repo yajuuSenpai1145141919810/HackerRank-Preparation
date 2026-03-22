a58b43be9ab0d75a23e6d09f05a87701dc7eb339
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-22 18:32:39 +0800

    Create ValidBracketSequences.cpp
    topic done

---
+++ Recursion/ValidBracketSequences.cpp
@@ -0,0 +1,6 @@
+// Generate Valid Angle Bracket Sequences
+// Given n, return all valid sequences of n pairs of '<' and '>' with proper nesting.
+// Input: n = 2 Output: <><> and <<>>
+
+//這題跟Validate Brackets那一題目差在 這題只有一種 所以不要用stack
+
