12a079cbce7ef0e44086541419c91351b6089e68
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-22 17:43:24 +0800

    Create RemoveDuplicates.cpp
    topic done

---
+++ linked list/RemoveDuplicates.cpp
@@ -0,0 +1,5 @@
+// Remove Consecutive Duplicates from Sorted Linked List
+// Write a function "deleteDuplicates" that removes consecutive duplicate nodes in-place, 
+// retaining only the first node of each code. Return the head of the resulting list.
+// Input head = [1, 2, 2, 2, 3, 4, 4, 5] Output= [1, 2, 3, 4, 5]
+
