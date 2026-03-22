8d3daa33df03391bca429026729d599b0d0190be
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-22 18:03:50 +0800

    Update RemoveDuplicates.cpp
    code done

--- linked list/RemoveDuplicates.cpp
+++ linked list/RemoveDuplicates.cpp
@@ -3,3 +3,20 @@
 // retaining only the first node of each code. Return the head of the resulting list.
 // Input head = [1, 2, 2, 2, 3, 4, 4, 5] Output= [1, 2, 3, 4, 5]
 
+SinglyLinkedListNode* deleteDuplicates(SinglyLinkedListNode* head) {
+    //edge cases
+    if(head==nullptr||head->next==nullptr){
+        return head;
+    }
+    SinglyLinkedListNode* current=head;
+    while(current!=nullptr&& current->next!=nullptr){
+        if(current->next->data==current->data){
+            current->next=current->next->next;
+        }else{
+            current=current->next;
+        }
+    }
+    return head;
+}
+
+
