8360d56b4ea7734271743a949f637a28f06c12eb
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-23 16:00:23 +0800

    Create PhoneDigits.cpp
    topic

---
+++ Recursion/PhoneDigits.cpp
@@ -0,0 +1,5 @@
+// Lexicographical Letter Combinations of Phone Digits
+// Given a string of digits where '2'-'9' map to letters (like on a phone keypad) and '0','1' map to themselves, 
+// return all possible letter combinations in lexicographical order.
+
+// 每一層都有多個選擇，且層數不固定->backtracking
