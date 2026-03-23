ab5d8892f6a689f1349f1f8fd29bb6f4e7c23b22
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-23 23:07:23 +0800

    Create nQueens.cpp
    topic done

---
+++ Recursion/nQueens.cpp
@@ -0,0 +1,4 @@
+// Place N Cameras Without Conflict on Blocked Grid
+// Given an NxN grid where 0 is empty and 1 is blocked, 
+// return true if N cameras can be placed on empty cells such that no two share the same row, column, or diagonal.
+// Input:N = 4, grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], Output: True
