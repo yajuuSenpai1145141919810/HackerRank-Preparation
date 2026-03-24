15b88b80e8d707a836fdc9022947d56ea75b071f
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-24 22:38:24 +0800

    Create Fibonacci.cpp
    寫完架構 先去洗澡

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,10 @@
+// Custom Fibonacci Sequence
+// Given n (0-based indexing), return the n-th Fibonacci number where F(0) = 1, F(1) = 2, and F(n) = F(n-1) + F(n-2).
+int fibo(int &n){
+    
+}
+long getAutoSaveInterval(int n) {
+    int result=fibo(n);
+    return result;
+
+}
