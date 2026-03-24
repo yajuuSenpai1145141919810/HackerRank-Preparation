2e8c58bf132b1e41e80a53c885a2870be9eaeac4
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-24 23:35:53 +0800

    Create Climbing Stairs.cpp
    topic done

---
+++ DP/Climbing Stairs.cpp
@@ -0,0 +1,3 @@
+// Ways to Fill Slots with Single or Double Coverage
+// Given n slots numbered 0 to n-1, return the number of ways to fill all slots where each operation covers either 1 slot or 2 adjacent slots.
+// 注意 這不能直接用fibo去算 因為這是string 所以用DP!
