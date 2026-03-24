79b1d0a9b960c04dff28dba88f5edcf4f3f54dcf
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-24 23:00:04 +0800

    Update Fibonacci.cpp
    done easy

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -1,10 +1,21 @@
 // Custom Fibonacci Sequence
 // Given n (0-based indexing), return the n-th Fibonacci number where F(0) = 1, F(1) = 2, and F(n) = F(n-1) + F(n-2).
-int fibo(int &n){
-    
-}
 long getAutoSaveInterval(int n) {
-    int result=fibo(n);
-    return result;
+    // 要設定成long
+    long temp=0;
+    long a=1;
+    long b=2;
+    if(n==0){
+        return 1;
+    }
+    if(n==1){
+        return 2;
+    }
+    for(int i=0;i<n-1;i++){
+        temp=a+b;
+        a=b;
+        b=temp;
+    }
+    return temp;
 
 }
