379512a46b2a785ca10baf6fdbcb846fe199491e
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-25 21:46:34 +0800

    Create BST height.cpp

---
+++ BST/BST height.cpp
@@ -0,0 +1,7 @@
+// Height of Binary Search Tree 
+// Given the root of a binary search tree, return the height of the tree. Height is the number of nodes along the longest path from root to leaf.
+// n = 7
+// values = [4, 2, 6, 1, 3, 5, 7]
+// leftChild = [1, 3, 5, -1, -1, -1, -1]
+// rightChild = [2, 4, 6, -1, -1, -1, -1]
+// Output:3
