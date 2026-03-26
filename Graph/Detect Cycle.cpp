287275963685c0aefeb990ca261195af93844498
Tim Harasaki (原崎厚儒) <harasakitim0418@gmail.com>
2026-03-26 15:31:20 +0800

    Create Detect Cycle.cpp

---
+++ Graph/Detect Cycle.cpp
@@ -0,0 +1,6 @@
+// Detect Cycle in Module Dependency Graph
+// Given n modules labeled 0 to n-1 and a list of directed edges dependencies where [u, v] means module u depends on module v,
+// return 1 if there is a cycle in the dependency graph, otherwise return 0.
+
+// Input:n = 4,dependencies = [[1, 0], [2, 1], [3, 2]] 
+// Output:0
